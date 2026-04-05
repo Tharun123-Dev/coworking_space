658083326fdce1ae6d8eb071942a900e948ced34
Tharun123-Dev <anjali.tharun9949@gmail.com>
2026-04-05 23:59:52 +0530

    new

--- Backend/coworkspace/chatbot/views.py
+++ Backend/coworkspace/chatbot/views.py
@@ -1,4 +1,4 @@
-import google.generativeai as genai
+from  google import genai
 from django.conf import settings
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
