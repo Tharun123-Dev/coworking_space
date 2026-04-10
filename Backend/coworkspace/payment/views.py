87538d1807581c7c19d4d8c74bf8c804a29e2e48
Tharun123-Dev <anjali.tharun9949@gmail.com>
2026-04-10 14:51:57 +0530

    new

--- Backend/coworkspace/payment/views.py
+++ Backend/coworkspace/payment/views.py
@@ -25,26 +25,17 @@ import razorpay
 from django.conf import settings
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-import hmac
-import hashlib
 
 @api_view(['POST'])
 def verify_payment(request):
 
     client = razorpay.Client(
-        auth=(settings.RAZORPAY_KEY_ID, settings.RAZORPAY_KEY_SECRET)
+        auth=(settings.RAZORPAY_KEY_ID, settings.RAZORPAY_SECRET)
     )
 
-    data = request.data
-
-    params_dict = {
-        'razorpay_order_id': data['razorpay_order_id'],
-        'razorpay_payment_id': data['razorpay_payment_id'],
-        'razorpay_signature': data['razorpay_signature']
-    }
-
     try:
-        client.utility.verify_payment_signature(params_dict)
+        client.utility.verify_payment_signature(request.data)
         return Response({"status": "success"})
-    except:
+    except Exception as e:
+        print("VERIFY ERROR:", e)
         return Response({"status": "failed"})
